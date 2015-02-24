1b600e792649d289a8e104da8a267ee6be0f5ecd
John Robbins <john@wintellect.com>
2015-02-24 15:52:24 -0800

    The CTP6 template nuget tools are broken so reverted to previous which work

--- Source/Wintellect.Analyzers/Wintellect.Analyzers/tools/install.ps1
+++ Source/Wintellect.Analyzers/Wintellect.Analyzers/tools/install.ps1
@@ -1,18 +1,6 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$analyzersPath = join-path $toolsPath "analyzers"
+$analyzerPath = join-path $toolsPath "analyzers"
+$analyzerFilePath = join-path $analyzerPath "Wintellect.Analyzers.dll"
 
-# Install the language agnostic analyzers.
-foreach ($analyzerFilePath in Get-ChildItem $analyzersPath)
-{
-    $project.Object.AnalyzerReferences.Add($analyzerFilePath.FullName)
-}
-
-# Install language specific analyzers.
-# $project.Type gives the language name like (C# or VB.NET)
-$languageAnalyzersPath = join-path $analyzersPath $project.Type
-
-foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath)
-{
-    $project.Object.AnalyzerReferences.Add($analyzerFilePath.FullName)
-}
+$project.Object.AnalyzerReferences.Add("$analyzerFilePath")
