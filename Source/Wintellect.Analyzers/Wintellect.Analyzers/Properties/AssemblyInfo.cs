e1b689d96fa03df6ecf592d8dde3eac87f6173bd
John Robbins <john@wintellect.com>
2015-03-11 15:21:02 -0700

    Moved extensions to own folder an dbumped version numbers

--- Source/Wintellect.Analyzers/Wintellect.Analyzers/Properties/AssemblyInfo.cs
+++ Source/Wintellect.Analyzers/Wintellect.Analyzers/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha1")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha6")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
